2288b21f702dca88e85e09b88d68539600aa884d
Doğukan Öztürkan <d.ozturkan@gmail.com>
2019-07-01 14:15:19 +0300

    First commit!

---
+++ src/main/java/com/dogukano/springbootpostgre/SpringBootPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.dogukano.springbootpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgreApplication.class, args);
+	}
+
+}
